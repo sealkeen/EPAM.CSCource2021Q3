c9dbbeb47b4447f90023f12381abef8b8c52bcd4
sealkeen <lettersforlotro@gmail.com>
2021-08-23 22:56:56 +0300

    23 august 2021, 22:56

---
+++ M01/ArrayHelper/ArrayTester/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ArrayHelper;
+using RectangleHelper;
+
+namespace ArrayTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArraySorter arraySorter = new ArraySorter();
+            ArraySummator arraySummator = new ArraySummator();
+            RectangleCalculator rectangleCalc = new RectangleCalculator();
+
+            var array = new int[5];
+            Random rnd = new Random();
+
+            for(int i = 0; i < array.Length; i++)
+                array[i] = rnd.Next(10);
+
+            Console.WriteLine("Random Int array:"); Show(array);
+            arraySorter.BubbleSort(array, false);
+            Console.WriteLine("Random Int array sorted (Descending): "); Show(array);
+
+            var twoDimArr = new int[,] { { 3, -2 }, { 7, -4 } };
+            Console.WriteLine("Two dim array: "); Show<int>(twoDimArr);
+
+            Console.WriteLine("Two dim array sum of positives: ");
+            Console.WriteLine(arraySummator.Sum(twoDimArr));
+
+            Console.WriteLine($"Perimeter of a rectangle 4, 5 <int>:, {rectangleCalc.Perimeter(4, 5)}");
+            Console.WriteLine($"Square of a rectangle 4, 5 <int>: {rectangleCalc.Square(4, 5)}");
+
+            Console.ReadKey();
+        }
+        static private void Show(Array array)
+        {
+            foreach (var e in array) {
+                Console.Write(e+" ");
+            }
+            Console.WriteLine();
+        }
+        static private void Show<T>(T[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++) {
+                for (int k = 0; k < array.GetLength(1); k++)
+                    Console.Write(array[i, k] + " ");
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ M01/ArrayHelper/RectangleHelper/RectangleHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RectangleHelper
+{
+    public class RectangleCalculator
+    {
+        public int Square(int width, int height)
+        {
+            return (width * height);
+        }
+        public int Perimeter(int width, int height)
+        {
+            return (width + height) * 2;
+        }
+    }
+}
