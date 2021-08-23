ac9e29cfc6f0eef96a7901d8ee3046100d8a5ce4
sealkeen <lettersforlotro@gmail.com>
2021-08-23 22:28:30 +0300

    23 august 2021, 22:28

--- M02/Students/Program.cs
+++ M02/Students/Program.cs
@@ -12,7 +12,7 @@ namespace Students
         Student(string email) {
             if (IsValidEmail(email))
             {
-                string[] fullName = email.Split('@')[0].Split('.');
+                var fullName = email.Split('@')[0].Split('.');
                 if (fullName.Length == 2)
                 {
                     FullName = fullName[0] + " " + fullName[1];
@@ -37,49 +37,49 @@ namespace Students
         }
     }
 
-    //TODO: In the main method create a string array "subjects" which contains 6 different shcool subjects("Maths, "PE", etc..).
-    //TODO: In the main method create 3 students with different names using first constructor
-
-    //TODO: (like var student1c1 = new Student("vasya.pupkin@epam.com")
-
-    //TODO: In the main method create 3 students with the same names names using second constructor
-
-
-    //TODO: (like var student1c2 = new Student("Vasya", "Pupkin").
-
-
-    //TODO: Overall you should have 3 unique students (but there are 2 instances of each student)
-    //TODO: Create a new empty dictionary of<Student, HashSet> called "studentSubjectDict".
-    //TODO: For each student(remebmer that we got 6 variables despite there are only 3 unique students) fill in the dictionary like
-
-
-    //TODO: studentSubjectDict[student1c1] = % 3 random values from the subject array%
-    //TODO: studentSubjectDict[student2c1] = %3 random values from the subject array%
-    //TODO: studentSubjectDict[student3c1] = %3 random values from the subject array%
-    //TODO: studentSubjectDict[student1c2] = %3 random values from the subject array%
-    //TODO: studentSubjectDict[student2c2] = %3 random values from the subject array%
-    //TODO: studentSubjectDict[student3c2] = %3 random values from the subject array%
-
-
-    //TODO: Make sure that after that there are only three records in the "studentSubjectDict" dictionary
-    //TODO: (for that purpose you should override Equals() and GetHashCode() for students class).
-
-    //TODO: Task2:
-    //TODO: Goal of the task is to get acquainted with Array.Sort, Stopwatch and System.Diagnostics.Process.
-
-    //TODO: Create a console application called "Performance".
-    //TODO: Create a class "C" with just one int field called "i".
-    //TODO: Create a struct "S" with just one int field called "i".
-    //TODO: In the main method create an array of 100000 "C" called "classes" and intialize it with random numbers.
-    //TODO: In the main method create an array of 100000 "S" called "structs" and intialize it with random numbers.
-    //TODO: Calculate PrivateMemorySize64 delta before and after arrays initialization (for each array separately). Print the results to console.
-    //TODO: Compare the difference between these deltas and print it to the console.
-    //TODO: Execute Array.Sort<С>(classes) и Array.Sort<S>(structs). Print the execution time of each sort to the console.
     class Program
     {
+        //TODO: In the main method create a string array "subjects" which contains 6 different shcool subjects("Maths, "PE", etc..).
+        //TODO: In the main method create 3 students with different names using first constructor
         static void Main(string[] args)
         {
+            var ;
             Console.WriteLine("Hello World!");
         }
+        //TODO: (like var student1c1 = new Student("vasya.pupkin@epam.com")
+
+        //TODO: In the main method create 3 students with the same names names using second constructor
+
+
+        //TODO: (like var student1c2 = new Student("Vasya", "Pupkin").
+
+
+        //TODO: Overall you should have 3 unique students (but there are 2 instances of each student)
+        //TODO: Create a new empty dictionary of<Student, HashSet> called "studentSubjectDict".
+        //TODO: For each student(remebmer that we got 6 variables despite there are only 3 unique students) fill in the dictionary like
+
+
+        //TODO: studentSubjectDict[student1c1] = % 3 random values from the subject array%
+        //TODO: studentSubjectDict[student2c1] = %3 random values from the subject array%
+        //TODO: studentSubjectDict[student3c1] = %3 random values from the subject array%
+        //TODO: studentSubjectDict[student1c2] = %3 random values from the subject array%
+        //TODO: studentSubjectDict[student2c2] = %3 random values from the subject array%
+        //TODO: studentSubjectDict[student3c2] = %3 random values from the subject array%
+
+
+        //TODO: Make sure that after that there are only three records in the "studentSubjectDict" dictionary
+        //TODO: (for that purpose you should override Equals() and GetHashCode() for students class).
+
+        //TODO: Task2:
+        //TODO: Goal of the task is to get acquainted with Array.Sort, Stopwatch and System.Diagnostics.Process.
+
+        //TODO: Create a console application called "Performance".
+        //TODO: Create a class "C" with just one int field called "i".
+        //TODO: Create a struct "S" with just one int field called "i".
+        //TODO: In the main method create an array of 100000 "C" called "classes" and intialize it with random numbers.
+        //TODO: In the main method create an array of 100000 "S" called "structs" and intialize it with random numbers.
+        //TODO: Calculate PrivateMemorySize64 delta before and after arrays initialization (for each array separately). Print the results to console.
+        //TODO: Compare the difference between these deltas and print it to the console.
+        //TODO: Execute Array.Sort<С>(classes) и Array.Sort<S>(structs). Print the execution time of each sort to the console.
     }
 }
