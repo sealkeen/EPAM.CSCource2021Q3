71b55712dfba0a3c73b51f24a52eb1101e0ddc02
sealkeen <lettersforlotro@gmail.com>
2021-08-24 10:34:43 +0300

    24 august 2021, 10:34 AM

--- M01/ArrayHelper/ArrayHelper/ArrayHelper.cs
+++ M01/ArrayHelper/ArrayHelper/ArrayHelper.cs
@@ -6,21 +6,6 @@ using System.Text;
 
 namespace ArrayHelper
 {
-    public class ArraySummator
-    {
-        public int Sum(int[,] array)
-        {
-            var sum = 0;
-            for (int i = 0; i < array.GetLength(0); i++) {
-                for (int k = 0; k < array.GetLength(1); k++) {
-                    if (array[i, k] > 0) {
-                        sum += array[i, k];
-                    }
-                }
-            }
-            return sum;
-        }
-    }
     public class ArraySorter
     {
         public void BubbleSort(int[] array, bool ascending = true)
