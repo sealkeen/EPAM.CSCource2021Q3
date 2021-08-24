9e2bcb0cdbbea69b72d056d801442c139aa8255a
sealkeen <lettersforlotro@gmail.com>
2021-08-24 15:00:31 +0300

    24 august 2021, 15:00 PM

--- M03/TextAndNumberDataConverter/Program.cs
+++ M03/TextAndNumberDataConverter/Program.cs
@@ -2,12 +2,11 @@
 
 namespace TextAndNumberDataConverter
 {
-
     //Create a console application
     //TODO: Create a class that implements a method which allows to define an average word length in an input string (returns double).
     //(Punctuation characters should not affect the word length.Don't use regex.)
     class WordLengthDetector {
-        private double DetectAverage( string source )
+        public double DetectAverage( string source )
         {
             throw new NotImplementedException();
         }
@@ -15,17 +14,36 @@ namespace TextAndNumberDataConverter
     //TODO: Create a class that implements a method that doubles in the first string parameter all characters belonging to the second string parameter.
     //(example: first parameter - "omg i love shrek", second parameter - "o kek", result - "oomg i loovee shreekk")
     class CharacterDuplicator {
-        private string DuplicateCharacters(string source)
+        public string DuplicateCharacters(string source)
         {
             throw new NotImplementedException();
         }
     }
-    //TODO: Create a class that implements a method, that returns the sum of two big numbers(bigger than long). The input numbers are strings and the function must return a string.
+    //TODO: Create a class that implements a method, that returns the sum of two big numbers(bigger than long).
+    //The input numbers are strings and the function must return a string.
     //(do not use long, big int and etc.)
+    class NumberSummator {
+        public string SumNumbers() {
+            throw new NotImplementedException();
+        }
+    } 
     //TODO: Create a class that implements a method which reverses all of the words within the string passed in.
     //(example: ReverseWords("The greatest victory is that which requires no battle") => "battle no requires which that is victory greatest The")
-    //TODO: Create a class that implements a method that takes a string which contains text with phone numbers with format like(+X (XXX) XXX-XX-XX or X XXX XXX-XX-XX or +XXX (XX) XXX-XXXX).
-    //(take the source from Text.txt file you should create yourself). A method should return a list of strings with that numbers and write them to another Numbers.txt file
+    class WordOrderReverser {
+        public string ReverseOrder(string source) {
+            throw new NotImplementedException();
+        }
+    }
+    //TODO: Create a class that implements a method that takes a string which contains text with phone numbers with format like
+    //(+X (XXX) XXX-XX-XX or X XXX XXX-XX-XX or +XXX (XX) XXX-XXXX).
+    //(take the source from Text.txt file you should create yourself).
+    //A method should return a list of strings with that numbers and write them to another Numbers.txt file
+    class PhoneNumberExtractor {
+        const int MaxPhoneNumberLength = 15;
+        public void ExtractPhoneNumbers() {
+            throw new NotImplementedException();
+        }
+    }
 
     //Example of Text.txt file:
     //Bla bla bla my number is +7 (921) 345-67-89 kekeke
