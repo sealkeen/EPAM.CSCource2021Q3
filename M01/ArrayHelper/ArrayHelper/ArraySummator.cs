1eecae709a8627fb2572562570beef71c6dabae2
sealkeen <lettersforlotro@gmail.com>
2021-08-24 10:38:03 +0300

    24 august 2021, 10:38 AM

---
+++ M01/ArrayHelper/ArrayHelper/ArraySummator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArrayHelper
+{
+    public class ArraySummator
+    {
+        public int Sum(int[,] array)
+        {
+            var sum = 0;
+            for (int i = 0; i < array.GetLength(0); i++) {
+                for (int k = 0; k < array.GetLength(1); k++) {
+                    if (array[i, k] > 0) {
+                        sum += array[i, k];
+                    }
+                }
+            }
+            return sum;
+        }
+    }
+}
