87e92cb5ef98bf07525a1a1e12c37717d136888c
sealkeen <lettersforlotro@gmail.com>
2021-08-24 20:29:52 +0300

    24 august 08:29 PM

---
+++ M04/Simple2DGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Simple2DGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
