264f320460f745047517c43215d015717461fa21
sealkeen <lettersforlotro@gmail.com>
2021-08-24 19:08:40 +0300

    24.08.2021 07:08 PM

--- M02/Students/Program.cs
+++ M02/Students/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Students
 {
@@ -39,49 +40,66 @@ namespace Students
 
     class Program
     {
+        private Random _rnd;
         static void Main(string[] args)
         {
+            Program program = new Program();
             //TODO: In the main method create a string array "subjects" which contains 6 different school subjects("Maths, "PE", etc..).
-            var subjects = new string[] { "Math", "Physics", "", "" };
+            var subjects = new string[] { "Math", "Physics", "Literature", "Chemistry", "Biology", "" };
 
             //TODO: In the main method create 3 students with different names using first constructor
-            Student[] students = new Student[] {  new Student("e"),   };
+            //TODO: (like var student1c1 = new Student("vasya.pupkin@epam.com")
+            var student1c1 = new Student("ilya.varlamov@epam.com");
+            var student2c1 = new Student("arseniy.pirozhkov@epam.com");
+            var student3c1 = new Student("irina.gavrilenkova@epam.com");
+
+            //TODO: In the main method create 3 students with the same names using second constructor
+            //TODO: (like var student1c2 = new Student("Vasya", "Pupkin").
+            var student1c2 = new Student("Ilya", "Varlamov");
+            var student2c2 = new Student("Arseniy", "Pirozhkov");
+            var student3c2 = new Student("Irina", "Gavrilenkova");
+
+            //TODO: Overall you should have 3 unique students (but there are 2 instances of each student)
+            //TODO: Create a new empty dictionary of<Student, HashSet> called "studentSubjectDict".
+            var studentSubjectDict = new Dictionary<Student, HashSet<string>>();
+
+            //TODO: For each student(remember that we got 6 variables despite there are only 3 unique students) fill in the dictionary like
+
+            studentSubjectDict[student1c1] = program.Generate3RandomSubjects(subjects);
+            studentSubjectDict[student2c1] = program.Generate3RandomSubjects(subjects);
+            studentSubjectDict[student3c1] = program.Generate3RandomSubjects(subjects);
+            studentSubjectDict[student1c2] = program.Generate3RandomSubjects(subjects);
+            studentSubjectDict[student2c2] = program.Generate3RandomSubjects(subjects);
+            studentSubjectDict[student3c2] = program.Generate3RandomSubjects(subjects);
+
+            //TODO: Make sure that after that there are only three records in the "studentSubjectDict" dictionary
+            //TODO: (for that purpose you should override Equals() and GetHashCode() for students class).
+
+            //TODO: Task2:
+            //TODO: Goal of the task is to get acquainted with Array.Sort, Stopwatch and System.Diagnostics.Process.
+
+            //TODO: Create a console application called "Performance".
+            //TODO: Create a class "C" with just one int field called "i".
+            //TODO: Create a struct "S" with just one int field called "i".
+            //TODO: In the main method create an array of 100000 "C" called "classes" and intialize it with random numbers.
+            //TODO: In the main method create an array of 100000 "S" called "structs" and intialize it with random numbers.
+            //TODO: Calculate PrivateMemorySize64 delta before and after arrays initialization (for each array separately). Print the results to console.
+            //TODO: Compare the difference between these deltas and print it to the console.
+            //TODO: Execute Array.Sort<С>(classes) и Array.Sort<S>(structs). Print the execution time of each sort to the console.
+
             Console.WriteLine("Hello World!");
         }
-        //TODO: (like var student1c1 = new Student("vasya.pupkin@epam.com")
 
-        //TODO: In the main method create 3 students with the same names using second constructor
-
-
-        //TODO: (like var student1c2 = new Student("Vasya", "Pupkin").
-
-
-        //TODO: Overall you should have 3 unique students (but there are 2 instances of each student)
-        //TODO: Create a new empty dictionary of<Student, HashSet> called "studentSubjectDict".
-        //TODO: For each student(remebmer that we got 6 variables despite there are only 3 unique students) fill in the dictionary like
+        private HashSet<string> Generate3RandomSubjects(string[] subjects) {
+            HashSet<string> hashSet = new HashSet<string>();
+            const int countOfStudents = 3;
+            for (int i = 0; i < countOfStudents; i++) {
+                hashSet.Add(subjects[_rnd.Next(0, 5)]);
+            }
+            return hashSet;
+        }
 
 
-        //TODO: studentSubjectDict[student1c1] = % 3 random values from the subject array%
-        //TODO: studentSubjectDict[student2c1] = %3 random values from the subject array%
-        //TODO: studentSubjectDict[student3c1] = %3 random values from the subject array%
-        //TODO: studentSubjectDict[student1c2] = %3 random values from the subject array%
-        //TODO: studentSubjectDict[student2c2] = %3 random values from the subject array%
-        //TODO: studentSubjectDict[student3c2] = %3 random values from the subject array%
 
-
-        //TODO: Make sure that after that there are only three records in the "studentSubjectDict" dictionary
-        //TODO: (for that purpose you should override Equals() and GetHashCode() for students class).
-
-        //TODO: Task2:
-        //TODO: Goal of the task is to get acquainted with Array.Sort, Stopwatch and System.Diagnostics.Process.
-
-        //TODO: Create a console application called "Performance".
-        //TODO: Create a class "C" with just one int field called "i".
-        //TODO: Create a struct "S" with just one int field called "i".
-        //TODO: In the main method create an array of 100000 "C" called "classes" and intialize it with random numbers.
-        //TODO: In the main method create an array of 100000 "S" called "structs" and intialize it with random numbers.
-        //TODO: Calculate PrivateMemorySize64 delta before and after arrays initialization (for each array separately). Print the results to console.
-        //TODO: Compare the difference between these deltas and print it to the console.
-        //TODO: Execute Array.Sort<С>(classes) и Array.Sort<S>(structs). Print the execution time of each sort to the console.
     }
 }
