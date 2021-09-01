0a886e0ecf1ba8eca8131eb26c4ee8cbd99f1f4f
sealkeen <lettersforlotro@gmail.com>
2021-09-01 14:50:07 +0300

    1 September 2021, 02:50 PM

---
+++ M05/ConsoleApplication/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ParserLibrary;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //NLog nLog = new NLog();
+            var parser = new Int32Parser();
+
+            Console.WriteLine(parser.Parse("-8765"));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ M05/ParserLibrary/Int32Parser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.RegularExpressions;
+namespace ParserLibrary
+{
+    public class Int32Parser
+    {
+        private const int _maxLengthInt32 = 10;
+        private const int digitASCIIOffest = 48;
+        public int Parse(string source) {
+            var trimmed = source.Trim();
+            var negative = false;
+            if ( !Regex.IsMatch(source, @"-?\d{1,10}" )) // {-9999999999 : 9999999999}
+                throw new ArgumentException();
+            if ( trimmed[0] == '-' )
+            {
+                negative = true;
+                trimmed = trimmed.Substring(1);
+            }
+            
+            var result = 0;
+            var lastIndex = trimmed.Length - 1;
+            for (int i = lastIndex; i >= 0; i--)
+            {
+                var power = (lastIndex - i);
+                var value  = trimmed[i] - digitASCIIOffest;
+                var tenInPower = Math.Pow(10, power);
+                result += (int)(value * tenInPower);
+            }
+
+            if (negative)
+                result *= -1;
+
+            return result;
+        }
+    }
+}
