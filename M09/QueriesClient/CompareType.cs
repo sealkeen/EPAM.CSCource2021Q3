8999e604a510433caf3b5eb6671bd250f2055281
sealkeen <lettersforlotro@gmail.com>
2021-09-27 11:18:01 +0300

    Added CompareType.cs

---
+++ M09/QueriesClient/CompareType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueriesClient
+{
+    enum CompareType
+    {
+        LessThan = -1, Equals = 0, MoreThan = 1 
+    }
+}
