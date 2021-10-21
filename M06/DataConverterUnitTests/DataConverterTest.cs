3db0a19047c0315e0911425cac130edc341824e5
sealkeen <lettersforlotro@gmail.com>
2021-10-21 19:04:40 +0300

    Refactored tests.

--- M06/DataConverterUnitTests/DataConverterTest.cs
+++ M06/DataConverterUnitTests/DataConverterTest.cs
@@ -8,104 +8,73 @@ namespace DataConverterUnitTests
     public class Tests
     {
         [Test]
-        public void AverageWordLengthTest()
+        [TestCase("One.Two three", 5.5)]
+        [TestCase("One. three!", 4)]
+        [TestCase("One. three! ,.four;", 4)]
+        [TestCase("One. three! ,.four;", 4)]
+        public void AverageWordLengthTest(string arg, double argResult)
         {
             //2 Second Task of M03
             var wordAverageLengthDetector = new WordAverageLengthDetector();
 
-            var length = wordAverageLengthDetector.GetAverageLength("One.Two three");
+            double length = wordAverageLengthDetector.GetAverageLength(arg);
             Debug.WriteLine(length);
-            Assert.AreEqual(length, 5.5);
-
-            length = wordAverageLengthDetector.GetAverageLength("One. three!");
-            Assert.AreEqual(length, 4);
-
-            length = wordAverageLengthDetector.GetAverageLength("One. three! ,.four;");
-            Assert.AreEqual(length, 4);
-
-            length = wordAverageLengthDetector.GetAverageLength("One. three! ,.four;");
-            Assert.AreEqual(length, 4);
+            Assert.AreEqual(length, argResult);
         }
         [Test]
-        public void CharacterDuplicatorTest()
+        [TestCase("How do you like this, Elon Mask?", "lask", "How do you llikke thiss, Ellon Maasskk?")]
+        [TestCase("first string", "st", "firsstt ssttring")]
+        public void CharacterDuplicatorTest(string source, string modifier, string expected)
         {
             //3 Third Task of M03
             var charDuplicator = new CharacterDuplicator();
-            var result = charDuplicator.GetMultipliedCharatersString("How do you like this, Elon Mask?", "lask");
+            var result = charDuplicator.GetMultipliedCharatersString(source, modifier);
             Debug.WriteLine(result);
-            Assert.AreEqual("How do you llikke thiss, Ellon Maasskk?", result);
-
-            result = charDuplicator.GetMultipliedCharatersString("first string", "st");
-            Debug.WriteLine(result);
-            Assert.AreEqual("firsstt ssttring", result);
+            Assert.AreEqual(expected, result);
         }
 
         [Test]
-        public void SumIntegersTest()
+        [TestCase("123", "456", "579")] [TestCase("1234", "4567", "5801")]
+        [TestCase("0000001234", "5567", "0000006801")] [TestCase("1", "2", "3")]
+        [TestCase("11", "2", "13")] 
+        [TestCase("1111111111111111111111111111", "2222222222222222222222222222", "3333333333333333333333333333")]
+        [TestCase("0000000000000009000000000000", "0000000000000001000000000000", "0000000000000010000000000000")]
+        [TestCase("000", "000", "000")] [TestCase("099", "011", "110")]
+        public void SumIntegersTest(string first, string second, string expected)
         {
             //4 Fourth Task of M03
             var nS = new NumberSummator();
-            var result = nS.SumInts("123", "456");
-            Assert.AreEqual(result, "579");
-
-            result = nS.SumInts("1234", "4567");
-            Assert.AreEqual(result, "5801");
-
-            result = nS.SumInts("0000001234", "5567");
-            Assert.AreEqual(result, "0000006801");
-
-            result = nS.SumInts("1", "2");
-            Assert.AreEqual(result, "3");
-
-            result = nS.SumInts("11", "2");
-            Assert.AreEqual(result, "13");
-
-            result = nS.SumInts("1111111111111111111111111111", "2222222222222222222222222222");
-            Assert.AreEqual(result, "3333333333333333333333333333");
-
-            result = nS.SumInts("0000000000000009000000000000", "0000000000000001000000000000");
-            Assert.AreEqual(result, "0000000000000010000000000000");
-
-            result = nS.SumInts("000", "000");
-            Assert.AreEqual(result, "000");
-
-            result = nS.SumInts("099", "011");
-            Assert.AreEqual(result, "110");
+            var result = nS.SumInts(first, second);
+            Assert.AreEqual(result, expected);
         }
 
         [Test]
-        public void SumDoubleTest()
+        [TestCase("123.0", "456", "579.0")]
+        [TestCase("123.0", "456", "579.0")]
+        [TestCase("124.05", "456", "580.05")]
+        [TestCase("12499999.05", "456", "12500455.05")]
+        [TestCase("5555555777776.0000058", "9823755088.000000005", "5565379532864.000005805")]
+        public void SumDoubleTest(string first, string second, string expected)
         {
             //4 Fourth Task of M03
             var nS = new NumberSummator();
-            var result = nS.SumNumbers("123.0", "456");
-            Assert.AreEqual(result, "579.0");
-
-            result = nS.SumNumbers("123.0", "456");
-            Assert.AreEqual(result, "579.0");
-
-            result = nS.SumNumbers("124.05", "456");
-            Assert.AreEqual(result, "580.05");
-
-            result = nS.SumNumbers("12499999.05", "456");
-            Assert.AreEqual(result, "12500455.05");
-
-            result = nS.SumNumbers("5555555777776.0000058", "9823755088.000000005");
-            Assert.AreEqual(result, "5565379532864.000005805");
+            var result = nS.SumNumbers(first, second);
+            Assert.AreEqual(expected, result);
         }
         [Test]
-        public void ReverseOrderTest()
+        [TestCase("The greatest victory is that which requires no battle")]
+        [TestCase("This is the second sentence in the method")]
+        [TestCase("This third sentence follows previous, the second one")]
+        public void ReverseOrderTest(string wordsToReverse)
         {
             //5 Fifth Task of M03
             var wordOrderReverser = new WordOrderReverser();
-            var wordsToReverse = "The greatest victory is that which requires no battle";
-            Debug.WriteLine($"\n{wordsToReverse} => {wordOrderReverser.ReverseOrder(wordsToReverse)}");
+            var aWLD = new WordAverageLengthDetector();
+            string result;
+            Console.WriteLine($"\n{wordsToReverse} => {result = wordOrderReverser.ReverseOrder(wordsToReverse)}");
 
-            wordsToReverse = "This is the second sentence in the method";
-            Debug.WriteLine($"\n{wordsToReverse} => {wordOrderReverser.ReverseOrder(wordsToReverse)}");
-
-            wordsToReverse = "This third sentence follows previous, the second one";
-            Debug.WriteLine($"\n{wordsToReverse} => {wordOrderReverser.ReverseOrder(wordsToReverse)}");
+            Assert.AreEqual(wordsToReverse.Length, result.Length);
+            Assert.AreEqual(aWLD.GetAverageLength(wordsToReverse),aWLD.GetAverageLength(result));
         }
 
         [SetUpFixture]
